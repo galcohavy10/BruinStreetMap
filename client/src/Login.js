31fc513ef5b9e51524d6f6f34c07493e15caa0b5
Jonathan Levi <jonathanlevi@ucla.edu>
2025-02-17 18:33:18 -0800

    Login.css and restriction to only ucla accounts

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,22 +1,37 @@
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from 'jwt-decode';
+import {useState } from "react";
+import "./Login.css";
 
 
 function Login({ onLogin }) {
   const handleLoginSuccess=(credentialResponse) => {
     const decodedUser = jwtDecode(credentialResponse.credential);
-    console.log(decodedUser);
-    onLogin(decodedUser);  
+    const userEmail = decodedUser.email;
+    
+    if(userEmail.endsWith("@g.ucla.edu")){
+      console.log("Authenticated:",userEmail);
+      onLogin(decodedUser);
+    }
+    else{
+      handleLoginFail();
+    }
+
   }
   const handleLoginFail=() => {
     console.log("Login Failed.");
   }
 
     return (
-    <div>
-        <h1>Login Page</h1>
-        <GoogleLogin onSuccess={handleLoginSuccess} onError={handleLoginFail}
-        />
+    <div className="login-container">
+      <div className="login-box">
+        <h1 className="login-title">Login</h1>
+        <p className="login-subtext">Sign in with your UCLA Google Account</p>
+
+        <div className="google-login-button">
+        <GoogleLogin onSuccess={handleLoginSuccess} onError={handleLoginFail} />
+        </div>
+      </div>
     </div>
   );
 }
